1c524a9ed249d62bfcedc036b553cb8a992baad7
Aiden Rocha <126905791+Arocha09@users.noreply.github.com>
2026-01-09 15:26:49 -0500

    For the other analyses

---
+++ app/instA.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+from scipy import stats
+from matplotlib.colors import TwoSlopeNorm
+import json
+import dash
+import geopandas as gpd
+from dash import Dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+from dash import dash_table
